b302595760116cc5e1622c190ec7b678a0f4b6b0
IntOflnxoverxdx <106513380+IntOflnxoverxdx@users.noreply.github.com>
2024-02-03 14:12:59 +1000

    1

--- scripts/search_post.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-
-const search_button = document.getElementById("search__button");
-const search_textfield = document.querySelector(".header__search__input");
-search_button.addEventListener("click",()=>{
-    window.location.replace(`catalog.html?${search_textfield.value}`);
-})
-}    
-)
