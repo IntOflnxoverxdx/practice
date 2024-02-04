8ebe983062f4c3de0e0bffbe82db63f9fa167a9e
IntOflnxoverxdx <106513380+IntOflnxoverxdx@users.noreply.github.com>
2024-02-04 15:25:30 +1000

    1

--- scripts/search.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let request = window.location.search.substring(9);
-    document.querySelector(".header__search__input").value = decodeURI(request);
-})
